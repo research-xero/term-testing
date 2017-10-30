c25cdb6018dae04e39624a9c9c40e5e69af9fbf7
research-xero <research.xero@gmail.com>
2017-10-30 22:21:34 +1100

    EOY script

--- eoy/validation-functions.js
+++ eoy/validation-functions.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
         var bv = $form.data('bootstrapValidator');
 
         // Use Ajax to submit form data
-        var url = 'https://script.google.com/macros/s/AKfycbxhEmEZoJYaAdRt9_RLik2_1DSEPLR2Qr469Q98SZnvA7EP_-yX/exec';
+        var url = 'https://script.google.com/macros/s/AKfycbyeHqGCmLl4RaTLF-7nRbGwVP6Z0Z8zh04yR8MHkoj2Pb0H1ohH/exec';
         var redirectUrl = './success-page.html';
         // show the loading
         $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
