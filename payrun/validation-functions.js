de29a05a66d7e0e855f7c3ea3479b1ce786828e0
research-xero <research.xero@gmail.com>
2017-10-30 22:09:48 +1100

    payrun script

--- payrun/validation-functions.js
+++ payrun/validation-functions.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
         var bv = $form.data('bootstrapValidator');
 
         // Use Ajax to submit form data
-        var url = 'https://script.google.com/macros/s/AKfycbxhEmEZoJYaAdRt9_RLik2_1DSEPLR2Qr469Q98SZnvA7EP_-yX/exec';
+        var url = 'https://script.google.com/macros/s/AKfycbyB1WZ_5TENgqN27ofLyf_g7mjZt5a1xtTuV110bvJKhquY7K-s/exec';
         var redirectUrl = './success-page.html';
         // show the loading
         $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
